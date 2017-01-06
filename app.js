aa14575b130ce68114e94051248695debb21c0cb
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-06 15:43:17 -0800

    multiply function passed

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-  return [45, 'The product of 5 and 9 is 45.'];
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result,message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
