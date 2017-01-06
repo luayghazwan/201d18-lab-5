70e044a1cd7c95d43b5f4baa5a33748952da39be
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-06 15:26:30 -0800

    Sum and Multiply passed

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a,b){ //eslint-disable-line
   var result = a + b;
-  var message = 'The sum of ' + a + ' and ' + b
- + ' is ' + result + '.';
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
   return [result, message];
 }
 
@@ -30,7 +29,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-  return [45, 'The product of 5 and 9 is 45.'];
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result,message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -51,11 +52,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumResult = testSum(a,b);
+  var mNsResult = testMultiply(sum,c);
+  return [sumResult,mNsResult,'4 and 7 and 5 sum to 16.','The product of 4 and 7 and 5 is 140.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
